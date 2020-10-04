3685127f403738e9aa19059cdfea1c4a09a34133
ishijimaitsuki <tatsuki445@gmail.com>
2020-10-04 21:04:56 +0900

    プロジェクト作成

---
+++ app/src/main/java/com/tatsuki/photom/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.tatsuki.photom
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Skeleton of an Android Things activity.
+ *
+ * Android Things peripheral APIs are accessible through the PeripheralManager
+ * For example, the snippet below will open a GPIO pin and set it to HIGH:
+ *
+ * val manager = PeripheralManager.getInstance()
+ * val gpio = manager.openGpio("BCM6").apply {
+ *     setDirection(Gpio.DIRECTION_OUT_INITIALLY_LOW)
+ * }
+ * gpio.value = true
+ *
+ * You can find additional examples on GitHub: https://github.com/androidthings
+ */
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
