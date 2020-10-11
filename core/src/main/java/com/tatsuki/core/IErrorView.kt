a288b0a365adae245cb86c899e6995c9e7852067
ishijimaitsuki <tatsuki445@gmail.com>
2020-10-11 21:37:23 +0900

    共通のViewロジックのInterface定義

---
+++ core/src/main/java/com/tatsuki/core/IErrorView.kt
@@ -0,0 +1,5 @@
+package com.tatsuki.core
+
+interface IErrorView {
+    fun showError(e: Exception)
+}
---
+++ core/src/main/java/com/tatsuki/core/ILoadingView.kt
@@ -0,0 +1,6 @@
+package com.tatsuki.core
+
+interface ILoadingView {
+    fun showLoading()
+    fun hideLoading()
+}
