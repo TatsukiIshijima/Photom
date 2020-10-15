8821e9f52487ceb530e12a2021ef3531dd994109
ishijimaitsuki <tatsuki445@gmail.com>
2020-10-15 22:45:50 +0900

    WIP:無限スライドショー

--- app/src/main/java/com/tatsuki/photom/ScreenSlidePagerAdapter.kt
+++ app/src/main/java/com/tatsuki/photom/ScreenSlidePagerAdapter.kt
@@ -3,16 +3,35 @@ package com.tatsuki.photom
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.adapter.FragmentViewHolder
+import androidx.viewpager2.widget.ViewPager2
 
-class ScreenSlidePagerAdapter(fragmentActivity: FragmentActivity)
+class ScreenSlidePagerAdapter(private val viewPager2: ViewPager2,
+                              fragmentActivity: FragmentActivity)
     : FragmentStateAdapter(fragmentActivity) {
 
     companion object {
         private const val NUM_PAGES = 5
     }
 
+    private val runnable = Runnable {
+        // TODO:スライドしたい項目を追加（疑似こーど）
+        // slideItems.addAll()
+        notifyDataSetChanged()
+    }
+
     override fun getItemCount(): Int = NUM_PAGES
 
-
     override fun createFragment(position: Int): Fragment = SlidePageFragment()
+
+    override fun onBindViewHolder(
+        holder: FragmentViewHolder,
+        position: Int,
+        payloads: MutableList<Any>
+    ) {
+        super.onBindViewHolder(holder, position, payloads)
+        if (position == NUM_PAGES - 2) {
+            viewPager2.post(runnable)
+        }
+    }
 }
--- app/src/main/java/com/tatsuki/photom/SlideShowFragment.kt
+++ app/src/main/java/com/tatsuki/photom/SlideShowFragment.kt
@@ -1,18 +1,32 @@
 package com.tatsuki.photom
 
 import android.os.Bundle
+import android.os.Handler
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
-import androidx.viewpager2.adapter.FragmentStateAdapter
 import androidx.viewpager2.widget.ViewPager2
 import kotlinx.android.synthetic.main.fragment_slide_show.*
 
 class SlideShowFragment : Fragment() {
 
     private lateinit var viewPager: ViewPager2
+
+    private val sliderHandler = Handler()
+
+    private val sliderRunnable = Runnable {
+        viewPager.currentItem += 1
+    }
+
+    private val onPageChangeCallback = object : ViewPager2.OnPageChangeCallback() {
+        override fun onPageSelected(position: Int) {
+            super.onPageSelected(position)
+            sliderHandler.removeCallbacks(sliderRunnable)
+            // スライドしている時間
+            sliderHandler.postDelayed(sliderRunnable, 3000)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,19 +36,21 @@ class SlideShowFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val pagerAdapter = ScreenSlidePagerAdapter(requireActivity())
-
         viewPager = view_pager2
+        val pagerAdapter = ScreenSlidePagerAdapter(viewPager, requireActivity())
         viewPager.adapter = pagerAdapter
+
+        // https://qiita.com/yfujiki/items/93ba588313ee0edbd8e9
+        // https://stackoverflow.com/questions/58735388/how-to-add-endless-infinite-scroll-to-viewpager2
+        // https://www.youtube.com/watch?v=iA9iqygq11Q
+        // 上の動画の10分あたり
+        // RecyclerViewとViewPager2の併用
+        viewPager.registerOnPageChangeCallback(onPageChangeCallback)
     }
 
-    private inner class ScreenSlidePagerAdapter(fragmentActivity: FragmentActivity): FragmentStateAdapter(fragmentActivity) {
-
-        private val numPages = 5
-
-        override fun getItemCount(): Int = numPages
-
-        override fun createFragment(position: Int): Fragment = SlidePageFragment()
-
+    override fun onDestroyView() {
+        super.onDestroyView()
+        viewPager.unregisterOnPageChangeCallback(onPageChangeCallback)
     }
 }
+
