5d8f45717221f286eee30e9a1ff9953d144aa248
ishijimaitsuki <tatsuki445@gmail.com>
2020-10-17 21:52:27 +0900

    スライドページの背景色をページごとに変更

--- app/src/main/java/com/tatsuki/photom/ScreenSlidePagerAdapter.kt
+++ app/src/main/java/com/tatsuki/photom/ScreenSlidePagerAdapter.kt
@@ -17,12 +17,21 @@ class ScreenSlidePagerAdapter(private val viewPager2: ViewPager2,
     private val runnable = Runnable {
         // TODO:スライドしたい項目を追加（疑似こーど）
         // slideItems.addAll()
-        notifyDataSetChanged()
+        // notifyDataSetChanged()
     }
 
     override fun getItemCount(): Int = NUM_PAGES
 
-    override fun createFragment(position: Int): Fragment = SlidePageFragment()
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> SlidePageFragment(android.R.color.holo_purple)
+            1 -> SlidePageFragment(android.R.color.holo_red_light)
+            2 -> SlidePageFragment(android.R.color.holo_orange_light)
+            3 -> SlidePageFragment(android.R.color.holo_green_light)
+            4 -> SlidePageFragment(android.R.color.holo_blue_light)
+            else -> SlidePageFragment(R.color.colorPrimary)
+        }
+    }
 
     override fun onBindViewHolder(
         holder: FragmentViewHolder,
--- app/src/main/java/com/tatsuki/photom/SlidePageFragment.kt
+++ app/src/main/java/com/tatsuki/photom/SlidePageFragment.kt
@@ -5,11 +5,19 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_slide_page.*
 
-class SlidePageFragment : Fragment() {
+class SlidePageFragment(
+    private val colorResource: Int
+) : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? = inflater.inflate(R.layout.fragment_slide_page, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        constraintLayout.setBackgroundColor(resources.getColor(colorResource, null))
+    }
 }
