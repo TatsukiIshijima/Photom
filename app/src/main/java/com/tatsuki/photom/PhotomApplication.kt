13bd3e4b92a66c54a63d1e0beceaa846e2147ade
ishijimaitsuki <tatsuki445@gmail.com>
2020-11-01 16:23:25 +0900

    コンテナクラス作成

--- app/src/main/java/com/tatsuki/photom/PhotomApplication.kt
+++ app/src/main/java/com/tatsuki/photom/PhotomApplication.kt
@@ -1,7 +1,8 @@
 package com.tatsuki.photom
 
 import android.app.Application
+import com.tatsuki.photom.container.PhotomContainer
 
 class PhotomApplication: Application() {
-
+    val photomContainer = PhotomContainer()
 }
---
+++ app/src/main/java/com/tatsuki/photom/container/PhotomContainer.kt
@@ -0,0 +1,4 @@
+package com.tatsuki.photom.container
+
+class PhotomContainer {
+}
