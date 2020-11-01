66516d8d3799adb7e4b94c0645029f34536ab0ea
ishijimaitsuki <tatsuki445@gmail.com>
2020-11-01 22:33:47 +0900

    生成のタイミングを修正

--- app/src/main/java/com/tatsuki/photom/PhotomApplication.kt
+++ app/src/main/java/com/tatsuki/photom/PhotomApplication.kt
@@ -4,5 +4,14 @@ import android.app.Application
 import com.tatsuki.photom.container.PhotomContainer
 
 class PhotomApplication: Application() {
-    val photomContainer = PhotomContainer()
+
+    lateinit var photomContainer: PhotomContainer
+
+    override fun onCreate() {
+        super.onCreate()
+
+        // Container内にFirebase依存があり、初期化前に読んでしまうと Initialize Error となるので
+        // ライフサイクルに合わせて Container を生成する
+        photomContainer = PhotomContainer()
+    }
 }
