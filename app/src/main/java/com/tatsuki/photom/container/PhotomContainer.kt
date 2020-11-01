72204177cf0edfb302add273d63b81674e4667ef
ishijimaitsuki <tatsuki445@gmail.com>
2020-11-01 16:30:27 +0900

    コンテナクラスに使用するインスタンスの生成

--- app/src/main/java/com/tatsuki/photom/container/PhotomContainer.kt
+++ app/src/main/java/com/tatsuki/photom/container/PhotomContainer.kt
@@ -1,4 +1,17 @@
 package com.tatsuki.photom.container
 
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import com.tatsuki.core.api.OpenWeatherApiClient
+import com.tatsuki.core.repository.SlideImageRepository
+import com.tatsuki.core.repository.WeatherRepository
+
 class PhotomContainer {
+
+    private val firebaseStorage = Firebase.storage
+
+    private val openWeatherApiClient = OpenWeatherApiClient()
+
+    val weatherRepository = WeatherRepository(openWeatherApiClient)
+    val slideImageRepository = SlideImageRepository(firebaseStorage)
 }
