3aa98d3d4aacaf483f7bfd6f05612413b59671de
ishijimaitsuki <tatsuki445@gmail.com>
2021-04-09 22:56:00 +0900

    HiltでDIで宣言できるよう修正

--- app/src/main/java/com/tatsuki/photom/view/weather/WeatherFragment.kt
+++ app/src/main/java/com/tatsuki/photom/view/weather/WeatherFragment.kt
@@ -5,14 +5,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.tatsuki.photom.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class WeatherFragment : Fragment() {
+
+    private val viewModel: WeatherViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.weather_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
     }
 }
--- app/src/main/java/com/tatsuki/photom/view/weather/WeatherViewModel.kt
+++ app/src/main/java/com/tatsuki/photom/view/weather/WeatherViewModel.kt
@@ -1,7 +1,10 @@
 package com.tatsuki.photom.view.weather
 
 import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class WeatherViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+@HiltViewModel
+class WeatherViewModel @Inject constructor() : ViewModel() {
+
 }
