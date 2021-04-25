11591c7d803672746ead25cec266bcdb4182e9c8
ishijimaitsuki <tatsuki445@gmail.com>
2021-04-25 20:21:30 +0900

    天気の詳細データのクラス作成

--- core/src/main/java/com/tatsuki/core/api/response/CurrentResponse.kt
+++ core/src/main/java/com/tatsuki/core/api/response/CurrentResponse.kt
@@ -3,6 +3,7 @@ package com.tatsuki.core.api.response
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 import com.tatsuki.core.entity.CurrentWeatherEntity
+import com.tatsuki.core.entity.CurrentWeatherInfoItem
 
 @JsonClass(generateAdapter = true)
 data class CurrentResponse(
@@ -19,10 +20,24 @@ data class CurrentResponse(
     @Json(name = "humidity")
     val humidity: Int,
 
+    @Json(name = "wind_speed")
+    val windSpeed: Int,
+
     @Json(name = "weather")
     val weather: List<WeatherResponse>
 )
 
 fun CurrentResponse.toCurrentWeatherEntity(): CurrentWeatherEntity {
-    return CurrentWeatherEntity(temp.toInt(), weather.firstOrNull()?.main, weather.firstOrNull()?.icon)
+    return CurrentWeatherEntity(
+        temp.toInt(),
+        weather.firstOrNull()?.main,
+        weather.firstOrNull()?.icon
+    )
+}
+
+fun CurrentResponse.toDetailItems(): List<CurrentWeatherInfoItem> {
+    val humidityItem = CurrentWeatherInfoItem.HumidityItem(humidity)
+    val pressureItem = CurrentWeatherInfoItem.PressureItem(pressure)
+    val windSpeedItem = CurrentWeatherInfoItem.WindSpeedItem(windSpeed)
+    return listOf(humidityItem, pressureItem, windSpeedItem)
 }
