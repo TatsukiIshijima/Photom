d35b7c568ae21ad7801a493e86ef61077d3cf5c9
ishijimaitsuki <tatsuki445@gmail.com>
2021-04-26 21:50:56 +0900

    現在の天気の詳細データを表示するAdapterを作成

---
+++ app/src/main/java/com/tatsuki/photom/view/weather/CurrentWeatherDetailAdapter.kt
@@ -0,0 +1,72 @@
+package com.tatsuki.photom.view.weather
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.tatsuki.core.entity.CurrentWeatherInfoItem
+import com.tatsuki.photom.R
+
+class CurrentWeatherDetailAdapter(
+
+) : ListAdapter<CurrentWeatherInfoItem, CurrentWeatherInfoViewHolder>(CurrentWeatherInfoDiffCallback) {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CurrentWeatherInfoViewHolder {
+        val view =
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.current_weather_detail_item, parent, false)
+        return CurrentWeatherInfoViewHolder(view, parent.context)
+    }
+
+    override fun onBindViewHolder(holder: CurrentWeatherInfoViewHolder, position: Int) {
+        val currentWeatherInfo = getItem(position)
+        holder.bind(currentWeatherInfo)
+    }
+}
+
+class CurrentWeatherInfoViewHolder(
+    view: View,
+    private val context: Context
+) : RecyclerView.ViewHolder(view) {
+    private val title: TextView = view.findViewById(R.id.detailTitle)
+    private val value: TextView = view.findViewById(R.id.detailValue)
+    private val unit: TextView = view.findViewById(R.id.detailUnit)
+
+    fun bind(currentWeatherInfoItem: CurrentWeatherInfoItem) {
+        when (currentWeatherInfoItem) {
+            is CurrentWeatherInfoItem.PressureItem -> {
+                title.text = context.resources.getText(R.string.pressure)
+                value.text = currentWeatherInfoItem.value.toString()
+                unit.text = context.resources.getText(R.string.pressure_unit)
+            }
+            is CurrentWeatherInfoItem.HumidityItem -> {
+                title.text = context.resources.getText(R.string.humidity)
+                value.text = currentWeatherInfoItem.value.toString()
+                unit.text = context.resources.getText(R.string.humidity_unit)
+            }
+            is CurrentWeatherInfoItem.WindSpeedItem -> {
+                title.text = context.resources.getText(R.string.wind_speed)
+                value.text = currentWeatherInfoItem.value.toString()
+                unit.text = context.resources.getText(R.string.wind_speed_unit)
+            }
+        }
+    }
+}
+
+object CurrentWeatherInfoDiffCallback : DiffUtil.ItemCallback<CurrentWeatherInfoItem>() {
+    override fun areItemsTheSame(
+        oldItem: CurrentWeatherInfoItem,
+        newItem: CurrentWeatherInfoItem
+    ): Boolean = oldItem == newItem
+
+    override fun areContentsTheSame(
+        oldItem: CurrentWeatherInfoItem,
+        newItem: CurrentWeatherInfoItem
+    ): Boolean = oldItem == newItem
+}
