dfd7b7179ef0d86333338740578cfab2a1695166
ishijimaitsuki <tatsuki445@gmail.com>
2021-04-29 15:01:19 +0900

    位置情報を取得してから天気情報を取得するよう修正

--- core/src/main/java/com/tatsuki/core/usecase/FetchCurrentWeatherUseCase.kt
+++ core/src/main/java/com/tatsuki/core/usecase/FetchCurrentWeatherUseCase.kt
@@ -2,36 +2,60 @@ package com.tatsuki.core.usecase
 
 import com.tatsuki.core.State
 import com.tatsuki.core.api.response.toCurrentWeatherEntity
+import com.tatsuki.core.repository.PlaceRepository
 import com.tatsuki.core.repository.WeatherRepository
 import com.tatsuki.core.usecase.ui.ICurrentWeatherView
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.*
 
 class FetchCurrentWeatherUseCase(
     private val currentWeatherView: ICurrentWeatherView,
-    private val weatherRepository: WeatherRepository
+    private val weatherRepository: WeatherRepository,
+    private val placeRepository: PlaceRepository
 ) {
 
     companion object {
         private val TAG = FetchCurrentWeatherUseCase::class.java.simpleName
     }
 
-    suspend fun execute(lat: Double, lon: Double) {
+    @FlowPreview
+    suspend fun execute() {
 
         currentWeatherView.showLoading()
 
-        weatherRepository.getWeather(lat = lat, lon = lon)
-            .collect { state ->
-                when (state) {
-                    is State.Success -> {
-                        currentWeatherView
-                            .showCurrentWeather(state.data.current.toCurrentWeatherEntity())
-                    }
-                    is State.Failed -> {
-                        currentWeatherView.showError(state.exception)
-                    }
+        // FIXME:もう少しきれいにつなげたい
+        placeRepository.fetchPlace()
+            .map {
+                when (it) {
+                    is State.Failed -> null
+                    is State.Success -> it.data
                 }
             }
-
-        currentWeatherView.hideLoading()
+            .filter {
+                if (it == null) {
+                    currentWeatherView.hideLoading()
+                    return@filter false
+                }
+                return@filter true
+            }
+            .filterNotNull()
+            .flatMapConcat {
+                weatherRepository.getWeather(
+                    lat = it.location.latitude,
+                    lon = it.location.longitude
+                )
+            }
+            .collect {
+                when (it) {
+                    is State.Success -> {
+                        currentWeatherView
+                            .showCurrentWeather(it.data.current.toCurrentWeatherEntity())
+                    }
+                    is State.Failed -> {
+                        currentWeatherView.showError(it.exception)
+                    }
+                }
+                currentWeatherView.hideLoading()
+            }
     }
 }
