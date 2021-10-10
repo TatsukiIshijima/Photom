171a133fdbe9d262adc464c078a350ad080a2c07
ishijimaitsuki <tatsuki445@gmail.com>
2021-10-10 17:25:36 +0900

    add entity

---
+++ data/entity/src/main/java/com/tatsuki/data/entity/PhotoEntity.kt
@@ -0,0 +1,6 @@
+package com.tatsuki.data.entity
+
+data class PhotoEntity(
+    val id: Int,
+    val url: String
+)
---
+++ data/entity/src/main/java/com/tatsuki/data/entity/PhotoListEntity.kt
@@ -0,0 +1,5 @@
+package com.tatsuki.data.entity
+
+data class PhotoListEntity(
+    val list: List<PhotoEntity>
+)
