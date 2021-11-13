a8f0cc719b1833bd81d568c48ab4545bd962e7e7
ishijimaitsuki <tatsuki445@gmail.com>
2021-11-13 23:16:13 +0900

    add extension

--- data/api/src/main/java/com/tatsuki/data/api/Result.kt
+++ data/api/src/main/java/com/tatsuki/data/api/Result.kt
@@ -1,10 +1,12 @@
 package com.tatsuki.data.api
 
 // https://medium.com/@douglas.iacovelli/how-to-handle-errors-with-retrofit-and-coroutines-33e7492a912
+// https://medium.com/swlh/kotlin-sealed-class-for-success-and-error-handling-d3054bef0d4e
+// https://github.com/seanghay/result-of/blob/master/resultof/src/main/java/com/seanghay/resultof/result-of-ktx.kt
 sealed class Result<out T> {
-    data class Success<out T>(
-        val data: T
-    ) : Result<T>()
+    data class Success<out R>(
+        val data: R
+    ) : Result<R>()
 
     data class Error(
         val code: Int? = null,
@@ -19,3 +21,28 @@ sealed class Result<out T> {
     object ServerError : Result<Nothing>()
     object NetworkError : Result<Nothing>()
 }
+
+inline fun <T, R> Result<T>.map(transform: (T) -> R): Result<R> =
+    when (this) {
+        is Result.Success -> Result.Success(transform(data))
+        is Result.ClientError -> this
+        is Result.Error -> this
+        Result.NetworkError -> Result.NetworkError
+        Result.ServerError -> Result.ServerError
+    }
+
+inline fun <T, R> Result<T>.flatMap(transform: (T) -> Result<R>): Result<R> =
+    when (this) {
+        is Result.Success -> transform(data)
+        is Result.ClientError -> this
+        is Result.Error -> this
+        Result.NetworkError -> Result.NetworkError
+        Result.ServerError -> Result.ServerError
+    }
+
+inline fun <T> Result<T>.withDefault(data: () -> T): Result.Success<T> =
+    when (this) {
+        is Result.Success -> this
+        else -> Result.Success(data())
+    }
+
