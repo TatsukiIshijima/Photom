da08acb1e6aa154334bc425edfa223d461138d37
ishijimaitsuki <tatsuki445@gmail.com>
2021-11-20 11:39:49 +0900

    create WeatherDetailFragment

---
+++ feature/weather/src/main/java/com/tatsuki/feature/weather/WeatherDetailFragment.kt
@@ -0,0 +1,33 @@
+package com.tatsuki.feature.weather
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.tatsuki.feature.weather.databinding.FragmentWeatherDetailBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class WeatherDetailFragment : Fragment() {
+
+    private var _binding: FragmentWeatherDetailBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentWeatherDetailBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
