38a2d830e4123bc295b99bcc5a627c507653fe3a
ishijimaitsuki <tatsuki445@gmail.com>
2021-11-21 20:36:43 +0900

    add binding methods

--- feature/weather/src/main/java/com/tatsuki/feature/weather/WeatherDetailFragment.kt
+++ feature/weather/src/main/java/com/tatsuki/feature/weather/WeatherDetailFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.tatsuki.data.entity.WeatherCondition
 import com.tatsuki.feature.weather.databinding.FragmentWeatherDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
 class WeatherDetailFragment : Fragment() {
@@ -27,6 +32,111 @@ class WeatherDetailFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        bind()
+
+        weatherDetailViewModel.fetchWeatherDetail()
+    }
+
+    private fun bind() {
+        weatherDetailViewModel.placeFlow
+            .onEach {
+                val locationName = if (it.isNotEmpty()) it else "---"
+                binding.locationName.text = locationName
+            }
+            .launchIn(lifecycleScope)
+
+        weatherDetailViewModel.conditionFlow
+            .onEach {
+                when (it) {
+                    is WeatherCondition.Atmosphere -> {
+                        binding.currentWeather.text = resources.getText(R.string.cloudy)
+                        binding.weatherDetailLayout.background =
+                            ResourcesCompat.getDrawable(
+                                resources,
+                                R.drawable.bg_cloudy,
+                                null
+                            )
+                    }
+                    is WeatherCondition.Clear -> {
+                        binding.currentWeather.text = resources.getText(R.string.sunny)
+                        binding.weatherDetailLayout.background =
+                            ResourcesCompat.getDrawable(
+                                resources,
+                                R.drawable.bg_sunny,
+                                null
+                            )
+                    }
+                    is WeatherCondition.Cloud -> {
+                        binding.currentWeather.text = resources.getText(R.string.cloudy)
+                        binding.weatherDetailLayout.background =
+                            ResourcesCompat.getDrawable(
+                                resources,
+                                R.drawable.bg_cloudy,
+                                null
+                            )
+                    }
+                    is WeatherCondition.Drizzle -> {
+                        binding.currentWeather.text = resources.getText(R.string.rainy)
+                        binding.weatherDetailLayout.background =
+                            ResourcesCompat.getDrawable(
+                                resources,
+                                R.drawable.bg_rainy,
+                                null
+                            )
+                    }
+                    is WeatherCondition.Rain -> {
+                        binding.currentWeather.text = resources.getText(R.string.rainy)
+                        binding.weatherDetailLayout.background =
+                            ResourcesCompat.getDrawable(
+                                resources,
+                                R.drawable.bg_rainy,
+                                null
+                            )
+                    }
+                    is WeatherCondition.Snow -> {
+                        binding.currentWeather.text = resources.getText(R.string.snow)
+                        binding.weatherDetailLayout.background =
+                            ResourcesCompat.getDrawable(
+                                resources,
+                                R.drawable.bg_snow,
+                                null
+                            )
+                    }
+                    is WeatherCondition.Thunderstorm -> {
+                        binding.currentWeather.text = resources.getText(R.string.rainy)
+                        binding.weatherDetailLayout.background =
+                            ResourcesCompat.getDrawable(
+                                resources,
+                                R.drawable.bg_rainy,
+                                null
+                            )
+                    }
+                    else -> {}
+                }
+            }
+            .launchIn(lifecycleScope)
+
+        weatherDetailViewModel.temperatureFlow
+            .onEach {
+                val value = it ?: "-"
+                val tempText =
+                    "${value}${requireContext().resources.getString(R.string.temperature_unit)}"
+                binding.currentTemp.text = tempText
+            }
+            .launchIn(lifecycleScope)
+
+        weatherDetailViewModel.weatherInfoListFlow
+            .onEach { }
+            .launchIn(lifecycleScope)
+
+        weatherDetailViewModel.timelyWeatherFlow
+            .onEach { }
+            .launchIn(lifecycleScope)
+
+        weatherDetailViewModel.dailyWeatherFlow
+            .onEach { }
+            .launchIn(lifecycleScope)
     }
 
     override fun onDestroyView() {
--- feature/weather/src/main/java/com/tatsuki/feature/weather/WeatherDetailViewModel.kt
+++ feature/weather/src/main/java/com/tatsuki/feature/weather/WeatherDetailViewModel.kt
@@ -1,6 +1,8 @@
 package com.tatsuki.feature.weather
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tatsuki.core.usecase.FetchWeatherDetailUseCase
 import com.tatsuki.core.usecase.ui.IErrorView
 import com.tatsuki.core.usecase.ui.ILoadingView
 import com.tatsuki.core.usecase.ui.IWeatherDetailView
@@ -9,20 +11,23 @@ import com.tatsuki.data.entity.DailyWeatherEntity
 import com.tatsuki.data.entity.TimelyWeatherEntity
 import com.tatsuki.data.entity.WeatherCondition
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class WeatherDetailViewModel @Inject constructor(
-
+    private val fetchWeatherDetailUseCase: FetchWeatherDetailUseCase
 ) : ViewModel() {
 
     private val loadingView = object : ILoadingView {
         override fun showLoading() {
-
+            mutableLoadingFlow.value = true
         }
 
         override fun hideLoading() {
-
+            mutableLoadingFlow.value = false
         }
     }
 
@@ -50,20 +55,52 @@ class WeatherDetailViewModel @Inject constructor(
         }
 
         override fun showCurrentWeather(condition: WeatherCondition, temp: Int) {
-
+            mutableConditionFlow.value = condition
+            mutableTemperatureFlow.value = temp
         }
 
         override fun showCurrentWeatherDetail(list: List<CurrentWeatherInfoItem>) {
-
+            mutableWeatherInfoListFlow.value = list
         }
 
         override fun showTimelyWeather(list: List<TimelyWeatherEntity>) {
-
+            mutableTimelyWeatherFlow.value = list
         }
 
         override fun showDailyWeather(list: List<DailyWeatherEntity>) {
-
+            mutableDailyWeatherFlow.value = list
         }
+    }
 
+    private val mutableLoadingFlow = MutableStateFlow(false)
+    val loadingFlow = mutableLoadingFlow.asStateFlow()
+
+    private val mutablePlaceFlow = MutableStateFlow("")
+    val placeFlow = mutablePlaceFlow.asStateFlow()
+
+    private val mutableConditionFlow = MutableStateFlow<WeatherCondition?>(null)
+    val conditionFlow = mutableConditionFlow.asStateFlow()
+
+    private val mutableTemperatureFlow = MutableStateFlow<Int?>(null)
+    val temperatureFlow = mutableTemperatureFlow.asStateFlow()
+
+    private val mutableWeatherInfoListFlow = MutableStateFlow(listOf<CurrentWeatherInfoItem>())
+    val weatherInfoListFlow = mutableWeatherInfoListFlow.asStateFlow()
+
+    private val mutableTimelyWeatherFlow = MutableStateFlow(listOf<TimelyWeatherEntity>())
+    val timelyWeatherFlow = mutableTimelyWeatherFlow.asStateFlow()
+
+    private val mutableDailyWeatherFlow = MutableStateFlow(listOf<DailyWeatherEntity>())
+    val dailyWeatherFlow = mutableDailyWeatherFlow.asStateFlow()
+
+    fun fetchWeatherDetail(locationName: String = "東京都渋谷区") {
+        viewModelScope.launch {
+            fetchWeatherDetailUseCase.execute(
+                loadingView,
+                errorView,
+                weatherView,
+                locationName
+            )
+        }
     }
 }
