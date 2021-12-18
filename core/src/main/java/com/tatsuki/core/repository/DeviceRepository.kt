58cffd73ea56e6aa5da73f75a78b2ac3ef3f394f
ishijimaitsuki <tatsuki445@gmail.com>
2021-12-18 09:14:05 +0900

    create DeviceRepository

---
+++ core/src/main/java/com/tatsuki/core/repository/DeviceRepository.kt
@@ -0,0 +1,22 @@
+package com.tatsuki.core.repository
+
+import com.tatsuki.data.api.ApiClient
+import com.tatsuki.data.api.Result
+import com.tatsuki.data.api.flatMap
+import com.tatsuki.data.api.switchbot.SwitchBotApi
+import com.tatsuki.data.api.switchbot.devicelist.response.DeviceListBodyResponse
+import javax.inject.Inject
+
+class DeviceRepository @Inject constructor(
+    private val switchBotApi: SwitchBotApi
+) {
+
+    suspend fun fetchDeviceList(): Result<DeviceListBodyResponse?> {
+        val result = ApiClient.safeApiCall({ switchBotApi.getDeviceList() })
+        return result
+            .flatMap {
+                if (it.statusCode == 200) Result.Success(it.body)
+                else Result.Error(it.statusCode, it.message)
+            }
+    }
+}
