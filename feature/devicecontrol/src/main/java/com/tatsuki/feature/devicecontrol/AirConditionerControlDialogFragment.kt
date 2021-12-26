3db1f43dfca3a7d042acbb7d2e20b376c55ebf9d
ishijimaitsuki <tatsuki445@gmail.com>
2021-12-26 21:53:58 +0900

    add CustomDialogFragments

---
+++ feature/devicecontrol/src/main/java/com/tatsuki/feature/devicecontrol/AirConditionerControlDialogFragment.kt
@@ -0,0 +1,12 @@
+package com.tatsuki.feature.devicecontrol
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+
+class AirConditionerControlDialogFragment : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState)
+    }
+}
---
+++ feature/devicecontrol/src/main/java/com/tatsuki/feature/devicecontrol/PowerControlDialogFragment.kt
@@ -0,0 +1,12 @@
+package com.tatsuki.feature.devicecontrol
+
+import android.app.Dialog
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+
+class PowerControlDialogFragment : DialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState)
+    }
+}
