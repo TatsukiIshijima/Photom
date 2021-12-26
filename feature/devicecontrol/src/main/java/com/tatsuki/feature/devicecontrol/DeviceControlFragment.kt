0fb82922d228b5f2fc72541a5cb2c9d31ccc609a
ishijimaitsuki <tatsuki445@gmail.com>
2021-12-26 20:55:49 +0900

    implemented DeviceControlFragment and DeviceControlViewModel

--- feature/devicecontrol/src/main/java/com/tatsuki/feature/devicecontrol/DeviceControlFragment.kt
+++ feature/devicecontrol/src/main/java/com/tatsuki/feature/devicecontrol/DeviceControlFragment.kt
@@ -6,8 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.tatsuki.feature.devicecontrol.databinding.FragmentDeviceControlBinding
+import com.xwray.groupie.GroupieAdapter
+import com.xwray.groupie.Section
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 
 @AndroidEntryPoint
 class DeviceControlFragment : Fragment() {
@@ -16,6 +24,7 @@ class DeviceControlFragment : Fragment() {
     private val binding get() = _binding!!
 
     private val deviceControlViewModel: DeviceControlViewModel by viewModels()
+    private val groupieAdapter = GroupieAdapter()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +32,59 @@ class DeviceControlFragment : Fragment() {
     ): View {
         _binding = FragmentDeviceControlBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.deviceControlGridView.apply {
+            layoutManager = GridLayoutManager(context, 3, RecyclerView.VERTICAL, false)
+            adapter = groupieAdapter
+        }
+
+        bind()
+
+        deviceControlViewModel.fetchSensorData()
+        deviceControlViewModel.fetchDeviceList()
+    }
+
+    private fun bind() {
+        deviceControlViewModel.loadingFlow
+            .onEach { }
+            .launchIn(lifecycleScope)
+
+        deviceControlViewModel.localDeviceListFlow
+            .filter { it.count() > 0 }
+            .onEach {
+                val title = resources.getString(R.string.device_item_section_local_device_title)
+                val section = Section()
+                section.setHeader(DeviceSectionItem(title))
+                val items = it.map { entity -> DeviceItem(entity) }
+                section.addAll(items)
+                groupieAdapter.add(section)
+            }
+            .launchIn(lifecycleScope)
+
+        deviceControlViewModel.remoteDeviceListFlow
+            .filter { it.count() > 0 }
+            .onEach {
+                val title =
+                    resources.getString(R.string.device_item_section_remote_device_section_title)
+                val section = Section()
+                section.setHeader(DeviceSectionItem(title))
+                val items = it.map { entity -> DeviceItem(entity) }
+                section.addAll(items)
+                groupieAdapter.add(section)
+            }
+            .launchIn(lifecycleScope)
+
+        deviceControlViewModel.sensorDataFlow
+            .onEach {
+                val section = Section()
+                section.setHeader(SensorDataHeaderItem(it))
+                groupieAdapter.add(0, section)
+            }
+            .launchIn(lifecycleScope)
     }
 
     override fun onDestroyView() {
--- feature/devicecontrol/src/main/java/com/tatsuki/feature/devicecontrol/DeviceControlViewModel.kt
+++ feature/devicecontrol/src/main/java/com/tatsuki/feature/devicecontrol/DeviceControlViewModel.kt
@@ -1,12 +1,53 @@
 package com.tatsuki.feature.devicecontrol
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.tatsuki.core.usecase.FetchDeviceListUseCase
+import com.tatsuki.core.usecase.FetchSensorDataUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DeviceControlViewModel @Inject constructor(
-
+    private val fetchSensorDataUseCase: FetchSensorDataUseCase,
+    private val fetchDeviceListUseCase: FetchDeviceListUseCase,
 ) : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    val loadingFlow = fetchDeviceListUseCase
+        .loadingView
+        .state
+        .mutableLoadingFlow
+        .asStateFlow()
+
+    val localDeviceListFlow = fetchDeviceListUseCase
+        .deviceListView
+        .state
+        .mutableLocalDeviceListFlow
+        .asStateFlow()
+
+    val remoteDeviceListFlow = fetchDeviceListUseCase
+        .deviceListView
+        .state
+        .mutableRemoteDeviceListFlow
+        .asStateFlow()
+
+    val sensorDataFlow = fetchSensorDataUseCase
+        .sensorDataView
+        .state
+        .mutableSensorDataFlow
+        .asStateFlow()
+
+    fun fetchSensorData() {
+        viewModelScope.launch {
+            fetchSensorDataUseCase.execute()
+        }
+    }
+
+    fun fetchDeviceList() {
+        viewModelScope.launch {
+            fetchDeviceListUseCase.execute()
+        }
+    }
 }
