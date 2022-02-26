122ae82c5ec2182ec7e7ac1c613861bc4086f80c
ishijimaitsuki <tatsuki445@gmail.com>
2022-02-26 16:39:51 +0900

    setup compose

--- feature/setting/src/main/java/com/tatsuki/setting/SettingFragment.kt
+++ feature/setting/src/main/java/com/tatsuki/setting/SettingFragment.kt
@@ -4,6 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.tatsuki.setting.databinding.FragmentSettingBinding
@@ -20,6 +27,22 @@ class SettingFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentSettingBinding.inflate(layoutInflater, container, false)
+            .apply {
+                greeting.setContent {
+                    Greeting()
+                }
+            }
         return binding.root
     }
+}
+
+@Preview
+@Composable
+private fun Greeting() {
+    Text(
+        text = "Hello World!!",
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentWidth(Alignment.CenterHorizontally)
+    )
 }
