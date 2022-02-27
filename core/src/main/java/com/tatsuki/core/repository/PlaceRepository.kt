fe458cad6ecb61596aceb6e0eb39db8f8adfd4fd
ishijimaitsuki <tatsuki445@gmail.com>
2022-02-27 10:51:47 +0900

    add fetch city method

--- core/src/main/java/com/tatsuki/core/repository/PlaceRepository.kt
+++ core/src/main/java/com/tatsuki/core/repository/PlaceRepository.kt
@@ -4,12 +4,20 @@ import com.tatsuki.data.api.ApiClient
 import com.tatsuki.data.api.Result
 import com.tatsuki.data.api.addresssearch.AddressSearchApi
 import com.tatsuki.data.api.addresssearch.response.AddressSearchResponse
+import com.tatsuki.data.api.citysearch.CitySearchApi
+import com.tatsuki.data.api.citysearch.response.CitySearchResponse
+import com.tatsuki.data.entity.PrefectureEntity
 import javax.inject.Inject
 
 class PlaceRepository @Inject constructor(
-    private val addressSearchApi: AddressSearchApi
+    private val addressSearchApi: AddressSearchApi,
+    private val citySearchApi: CitySearchApi
 ) {
 
     suspend fun fetchAddress(locationName: String): Result<List<AddressSearchResponse>> =
         ApiClient.safeApiCall({ addressSearchApi.getAddress(locationName) })
+
+    suspend fun fetchCity(prefectureEntity: PrefectureEntity): Result<CitySearchResponse> {
+        return ApiClient.safeApiCall({ citySearchApi.getCity(prefectureEntity.code) })
+    }
 }
