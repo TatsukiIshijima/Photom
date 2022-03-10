52034cf5bd99ee92f69b6660f2f64e3a8951c4fd
ishijimaitsuki <tatsuki445@gmail.com>
2022-03-10 23:10:37 +0900

    add save and get method

--- core/src/main/java/com/tatsuki/core/repository/PlaceRepository.kt
+++ core/src/main/java/com/tatsuki/core/repository/PlaceRepository.kt
@@ -7,17 +7,57 @@ import com.tatsuki.data.api.addresssearch.response.AddressSearchResponse
 import com.tatsuki.data.api.citysearch.CitySearchApi
 import com.tatsuki.data.api.citysearch.response.CitySearchResponse
 import com.tatsuki.data.entity.AddressEntity
+import com.tatsuki.data.entity.GeoLocationEntity
+import com.tatsuki.datasource.LocationDataSource
 import javax.inject.Inject
 
 class PlaceRepository @Inject constructor(
     private val addressSearchApi: AddressSearchApi,
-    private val citySearchApi: CitySearchApi
+    private val citySearchApi: CitySearchApi,
+    private val locationDataSource: LocationDataSource,
 ) {
+
+    var prefectureCash: AddressEntity.Prefecture? = null
+        private set
+    var cityCash: AddressEntity.City? = null
+        private set
 
     suspend fun fetchAddress(locationName: String): Result<List<AddressSearchResponse>> =
         ApiClient.safeApiCall({ addressSearchApi.getAddress(locationName) })
 
     suspend fun fetchCity(prefecture: AddressEntity.Prefecture): Result<CitySearchResponse> {
         return ApiClient.safeApiCall({ citySearchApi.getCity(prefecture.code) })
+    }
+
+    suspend fun getGeoLocation(): GeoLocationEntity {
+        return locationDataSource.getGeoLocation()
+    }
+
+    suspend fun saveGeoLocation(geoLocationEntity: GeoLocationEntity) {
+        locationDataSource.setGeoLocation(geoLocationEntity)
+    }
+
+    fun cashPrefecture(prefecture: AddressEntity.Prefecture) {
+        prefectureCash = prefecture
+    }
+
+    suspend fun getPrefecture(): AddressEntity.Prefecture {
+        return locationDataSource.getPrefecture()
+    }
+
+    suspend fun savePrefecture(prefecture: AddressEntity.Prefecture) {
+        locationDataSource.setPrefecture(prefecture)
+    }
+
+    fun cashCity(city: AddressEntity.City) {
+        cityCash = city
+    }
+
+    suspend fun getCity(): AddressEntity.City {
+        return locationDataSource.getCity()
+    }
+
+    suspend fun saveCity(city: AddressEntity.City) {
+        locationDataSource.setCity(city)
     }
 }
